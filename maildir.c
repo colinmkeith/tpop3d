a1602dcd3b0dec46299a29abe0f3043e82d78dc0
Chris Lightfoot <chris@ex-parrot.com>
2001-09-27 00:06:12 +0000

    ""

--- maildir.c
+++ maildir.c
@@ -80,6 +80,8 @@ int maildir_build_index(mailbox M, const char *subdir, time_t time) {
             struct indexpoint pt;
             maildir_make_indexpoint(&pt, filename, st.st_size, st.st_mtime);
             mailbox_add_indexpoint(M, &pt);
+            /* Hack: accumulate size of messages. */
+            M->st.st_size += st.st_size;
         }
         free(filename);
     }
