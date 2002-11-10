544733bc043a21c95c3172f9c50797760332477a
Chris Lightfoot <chris@ex-parrot.com>
2002-11-10 13:30:12 +0000

    ""

---
+++ tls.h
@@ -0,0 +1,20 @@
+/*
+ * tls.h:
+ * TLS stuff for tpop3d.
+ *
+ * Copyright (c) 2002 Chris Lightfoot. All rights reserved.
+ * Email: chris@ex-parrot.com; WWW: http://www.ex-parrot.com/~chris/
+ *
+ * $Id$
+ *
+ */
+
+#ifndef __TLS_H_ /* include guard */
+#define __TLS_H_
+
+/* tls.c */
+int tls_init(void);
+SSL_CTX *tls_create_context(const char *certfile, const char *pkeyfile);
+void tls_close(void);
+
+#endif /* __TLS_H_ */
