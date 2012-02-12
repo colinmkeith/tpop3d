ae0c8b3372ca10718c68f767944cbce3928573d7
Arkadiusz Miśkiewicz <arekm@maven.pl>
2012-02-12 08:22:44 +0100

    buffer_consume_to_mark: simplify to avoid accessing unallocated memory
    buffer_consume_to_mark() was trying to use Boyer-Moore search to find
    specified mark string but implementation was walking through unallocated
    mamory.
    
    tpop3d only use case is to look for single character mark, so simplify
    code for our use case.
    
    Fixes bug #33413.

--- connection.c
+++ connection.c
@@ -316,7 +316,7 @@ pop3command connection_parsecommand(connection c) {
      * latter case we must be careful not to interpret command1\ncommand2\r\n
      * as a single command. So always use \n as the line ending and strip off
      * any trailing \r. */
-    if (!(p = buffer_consume_to_mark(c->rdb, "\n", 1, line, &llen)))
+    if (!(p = buffer_consume_to_mark(c->rdb, "\n", line, &llen)))
         return NULL;
     else
         line = p;
